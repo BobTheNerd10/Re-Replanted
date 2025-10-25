7ee53f275b5d924a99ff6221c4b0b8516977fcf4
BobTheNerd10 <48499877+BobTheNerd10@users.noreply.github.com>
2025-10-25 15:32:22 -0700

    [Broken] Begin Loadingscreen Fix

--- source/extra-polish/Fixes/LoadingScreen.cs
+++ source/extra-polish/Fixes/LoadingScreen.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using MelonLoader;
 
 namespace extra_polish.Fixes
 {
@@ -11,24 +12,49 @@ namespace extra_polish.Fixes
     {
         public static void Implement()
         {
-            // TODO, subscribe to the scene initialization event and do the things
+            MelonEvents.OnSceneWasLoaded.Subscribe(OnSceneWasLoaded);
         }
 
-        public static void OnSceneWasInitialized(int buildIndex, string sceneName)
+        public static void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
+
+            if (sceneName != "Bootstrap") return; // Only run this code on the loading screen
+
+            // Get the loading bar, and then its children
+            // NOTE: Apparently, it's better to iterate through gameobjects based on their components.
+            // I think it's kinda silly, but whatevs, standards are standards
             GameObject LoadBar = GameObject.Find("LoadBarAnimationParent");
 
+            UnityEngine.Animator[] Animators = LoadBar.GetComponentsInChildren<UnityEngine.Animator>();
 
+            // Iterate through the loadbar components to find the ones that need fixing, and then fix them
+            foreach (UnityEngine.Animator LoadBarAnimator in Animators)
+            {
+                var Xposition = LoadBarAnimator.gameObject.transform.localPosition.x;
+                Vector3 objPosition;
+
+                switch (Xposition)
+                {
+                    case 155:
+                        MelonLogger.Msg("Implementing sprout fix...");
+                        objPosition = LoadBarAnimator.gameObject.transform.localPosition;
+                        objPosition.x = 185; // Change to the correct position
+                        LoadBarAnimator.gameObject.transform.localPosition = objPosition;
+
+                        break;
+
+                    case 290:
+                        MelonLogger.Msg("Implementing head fix...");
+                        objPosition = LoadBarAnimator.gameObject.transform.localPosition;
+                        objPosition.x = 400; // Change to the correct position
+                        LoadBarAnimator.gameObject.transform.localPosition = objPosition;
+
+                        break;
+                }
+            }
             /*
             TODO: Write code here that iterates through the Load Bar and fixes all its problems
-          
-            Sprout and Zombie positions 
-            -465
-            -245
-            -18
-            155 (needs to be changed)
-            290 (needs to be changed)
-
+         
             The timing of the 4th and 5th object will also likely need to be changed to match the new positions
 
             The zombie head animation is also quite choppy 
